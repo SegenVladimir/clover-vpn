cf9f9dd26642afd4ce3a94925afada05d86f76a6
Vladimir Segen <vladimirsegen@MacBook-Pro-Vladimir.local>
2024-01-05 18:32:35 +0300

    init

---
+++ src/components/App/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import LoadScreen from '../LoadScreen/LoadScreen.vue'
+</script>
+<style lang="scss">
+@import "App";
+</style>
+<template>
+  <div class="app">
+      <div class="app__header">
+          <div class="app__time">18:00</div>
+          <div class="app__status">
+              <div class="app__wifi"></div>
+              <div class="app__signal"></div>
+              <div class="app__battery"></div>
+          </div>
+      </div>
+      <div class="app__content">
+          <LoadScreen />
+      </div>
+      <div class="app__bottom"></div>
+  </div>
+</template>
---
+++ src/components/LoadScreen/LoadScreen.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+<style lang="scss">
+@import "LoadScreen.scss";
+</style>
+
+<template>
+    <div class="load-screen">
+        <img src="/logo.svg" alt=""/>
+    </div>
+</template>
