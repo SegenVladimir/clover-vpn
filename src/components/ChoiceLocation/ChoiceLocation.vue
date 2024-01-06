81cdd373046369071ec39fd1db98cfed09272044
Vladimir Segen <vladimirsegen@MacBook-Pro-Vladimir.local>
2024-01-07 00:36:45 +0300

    added search element

--- src/components/ChoiceLocation/ChoiceLocation.vue
+++ src/components/ChoiceLocation/ChoiceLocation.vue
@@ -1,10 +1,27 @@
 <script setup>
 import ListLocation from "../ListLocation/ListLocation.vue";
+import {ref} from "vue";
 
 const props = defineProps({
     selectedLocation: String
 });
 const emit = defineEmits(['openChoiceLocation', 'selectedLocation']);
+const search = ref(null);
+const searchActive = ref(false);
+
+
+const onShowSearch = () => {
+    const activeRevers = !searchActive.value;
+    searchActive.value = activeRevers;
+
+    if(activeRevers){
+        setTimeout(() => {
+            search.value.focus();
+        }, 0);
+    }
+};
+
+;
 </script>
 <style lang="scss">
 @import "ChoiceLocation";
@@ -14,10 +31,10 @@ const emit = defineEmits(['openChoiceLocation', 'selectedLocation']);
         <div class="choice-location__header">
             <button class="choice-location__close" @click.prevent.stop="emit('openChoiceLocation', false);">close</button>
             <div class="choice-location__title">Virtual Location</div>
-            <button type="button" class="choice-location__search-btn">search</button>
-            <div class="choice-location__search">
-                <input type="text" class="choice-location__input" placeholder="Enter Location"/>
-                <button type="button" class="choice-location__search-close">close search</button>
+            <button type="button" class="choice-location__search-btn" @click.prevent.stop="onShowSearch">search</button>
+            <div v-if="searchActive" class="choice-location__search">
+                <input ref="search" name="search" type="text" autocomplete="off" class="choice-location__input" placeholder="Enter Location"/>
+                <button type="button" class="choice-location__search-close" @click.prevent.stop="onShowSearch">close search</button>
             </div>
         </div>
         <div class="choice-location__content">
